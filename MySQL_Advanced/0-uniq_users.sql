1919e4ce4d6255f813c0b5e1bfbc7b09acd99b26
Iorsh07 <140644607+Iorsh07@users.noreply.github.com>
2026-05-05 18:33:24 -0600

    Modify users table creation with unique email constraint
    Updated the users table creation to include IF NOT EXISTS and set the primary key directly.

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
-// zz
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
+-- Create table users with unique email
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
-    PRIMARY KEY (id)
+    UNIQUE(email)
 );
